453d5272a6f53ff913d27f1aecb08bb5d0c8c323
gbanm <gbanm06@gmail.com>
2021-11-19 23:56:14 +0100

    array_to_latex

--- Qiskit example.py
+++ Qiskit example.py
@@ -1,6 +1,6 @@
 from qiskit import QuantumCircuit, Aer, execute
 from qiskit.quantum_info import Statevector
-from qiskit.visualization import plot_bloch_multivector, plot_histogram
+from qiskit.visualization import plot_bloch_multivector, plot_histogram, array_to_latex
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 def print_bloch_sphere(qc):
@@ -12,6 +12,10 @@ def print_plot_histogram(qc):
     backend = Aer.get_backend('qasm_simulator')
     job = execute(qc, backend)
     return plot_histogram(job.result().get_counts(), color='midnightblue', title="New Histogram")
+
+def print_state_vector(qc):
+    final_state = Statevector.from_instruction(qc)
+    return array_to_latex(final_state, prefix="\\text{Statevector} = ")
 
 qc = QuantumCircuit(2)
 qc.x(0)
